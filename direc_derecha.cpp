f5c294bca74419979d72a3532046099e8a3561c8
yngomeza <yngomeza@unjbg.edu.pe>
2026-09-24 15:54:56 -0500

    Agregando el ejercicio de ordenación directa por la derecha

---
+++ direc_derecha.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Ingrese el tamaño del arreglo: ";
+    cin >> n;
+    cout << endl;
+
+    int* arr = new int[n];
+
+    cout << "Ingrese los " << n << " elementos del arreglo:" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << "Elemento [" << i + 1 << "]: ";
+        cin >> arr[i];
+    }
+
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int aux = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = aux;
+            }
+        }
+    }
+
+    cout << "\nArreglo ordenado (por la derecha): ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    delete[] arr;
+
+    return 0;
+}
